3273c3df1b53311fb37bf25d9e9b680299c518df
watermelonsuperman <15538287230@163.com>
2019-08-28 17:27:16 +0800

    mockMVC test

--- src/main/java/com/oocl/web/sampleWebApp/HomeController.java
+++ src/main/java/com/oocl/web/sampleWebApp/HomeController.java
@@ -1,8 +1,15 @@
 package com.oocl.web.sampleWebApp;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
 public class HomeController {
@@ -11,4 +18,23 @@ public class HomeController {
     public @ResponseBody String greeting() {
         return "Hello World";
     }
+    @RequestMapping("/hello")
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public @ResponseBody String greetingBad() {
+        return "Hello World";
+    }
+    @RequestMapping("/user")
+    public @ResponseBody Map getName(){
+    	Map user = new HashMap();
+    	user.put("w", "y");
+    	return user;
+    }
+    @PostMapping("/users")
+    @ResponseStatus(HttpStatus.CREATED)
+
+    public @ResponseBody Map addId(@RequestBody HashMap map){
+    	map.put("id", "0001");
+    	return map;
+    }
+    
 }
--- src/test/java/com/oocl/web/sampleWebApp/HomeControllerTest.java
+++ src/test/java/com/oocl/web/sampleWebApp/HomeControllerTest.java
@@ -6,13 +6,22 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.util.HashMap;
+
+import org.hamcrest.CoreMatchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -26,5 +35,41 @@ public class HomeControllerTest {
     public void shouldReturnDefaultMessage() throws Exception {
         this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
                 .andExpect(content().string(containsString("Hello World")));
+    }
+    
+    @Test
+    public void shouldReturnMessage() throws Exception {
+        this.mockMvc.perform(get("/hello")).andDo(print()).andExpect(status().isBadRequest())
+                .andExpect(content().string(containsString("Hello World")));
+    }
+    @Test
+    public void shouldGetNameInBody() throws Exception {
+        this.mockMvc.perform(get("/user"))
+        		.andDo(print())
+        		.andExpect(
+        				status().isOk()
+        				)
+                .andExpect(
+                		MockMvcResultMatchers.jsonPath("$.w", CoreMatchers.is("y"))
+                		);
+    }
+    @Autowired
+    private ObjectMapper objectMapper;
+    @Test
+    public void shouldGeneratedIdInBody() throws Exception{
+    	HashMap map = new HashMap();
+    	String postString = objectMapper.writeValueAsString(map);
+    	
+    	this.mockMvc.perform(
+    			MockMvcRequestBuilders
+    			.post("/users")
+    			.contentType(MediaType.APPLICATION_JSON)
+    			.content(postString)
+    			)
+    			.andDo(print())
+    			.andExpect(status().isCreated())
+    			.andExpect(
+                		MockMvcResultMatchers.jsonPath("$.id", CoreMatchers.is("0001"))
+    					);
     }
 }
